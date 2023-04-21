bde6a982ea1bc043c9ee4d055fbb3fb2b36610bf
Vlad <Vlad Bakh>
2023-04-21 22:41:59 +0300

    added label

--- NamesCompApp/HomeViewController.swift
+++ NamesCompApp/HomeViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    let label: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.text = "Hello"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(label)
+        label.center = view.center
+        view.backgroundColor = .systemBackground
         
-        view.backgroundColor = .link
     }
 }
